056ce5922bae6970fe548c7cdb9349fd1f4bc013
Magne Rayan <rayan.magne@supdevinci-edu.fr>
2025-12-03 11:55:57 +0100

    feat: correction appel de la bdd

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from db_init import get_database
 import sys
-from game import  lancer_game
-
-from pymongo import MongoClient
-
-def get_database(db):
-
-    return db
+from game import lancer_game
 
 def afficher_scores():
     print("Affichage des scores...")
 
 def main():
-    db = get_database()
+    db = get_database() 
 
     while True:
         print("=== Menu ToDo ===")
@@ -23,10 +17,10 @@ def main():
         choice = input("Entrez votre choix : ")
 
         if choice == '1':
-            lancer_game()
+            lancer_game(db)
 
         elif choice == "2":
-            afficher_scores()
+            afficher_scores(db)
 
         elif choice == "3":
             print("Fermeture de l’application. À bientôt !")
@@ -37,4 +31,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
