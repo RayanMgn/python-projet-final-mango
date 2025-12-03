d4e13644ce0f1d816440d543fbb9f7a0cededd3c
Magne Rayan <rayan.magne@supdevinci-edu.fr>
2025-12-03 12:24:37 +0100

    feat(models.py) : creation des class Personnage et Monstres

---
+++ models.py
@@ -0,0 +1,17 @@
+class Personnage:
+    def __init__(self, _id=None, nom="", ATK=0, DEF=0, PV=0):
+        self.id = _id
+        self.nom = nom
+        self.ATK = ATK
+        self.DEF = DEF
+        self.PV = PV
+
+
+
+class Monstre:
+    def __init__(self, _id=None, nom="", ATK=0, DEF=0, PV=0):
+        self.id = _id
+        self.nom = nom
+        self.ATK = ATK
+        self.DEF = DEF
+        self.PV = PV  
